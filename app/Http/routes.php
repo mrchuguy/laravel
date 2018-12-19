2b4d9e117d8a0c52ad6b8532f0a17315c48bec98
user <Global@mail.ua>
2018-12-19 17:49:11 +0200

    много изменений

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,27 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use Illuminate\Http\Request;
+use App\Task;
 
 Route::get('/', function () {
-    return view('welcome');
+    $task = new Task();
+    return view('tasks', [
+	'tasks'=>$task->all()
+    ]);
+});
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')
+      ->withInput()
+      ->withErrors($validator);
+  }
+  
+  $new_task = new Task();
+  $new_task->name = $request->name;
+  $new_task->save();
+  return redirect('/');
 });
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Laravel Quickstart - Basic</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+    <!-- CSS и JavaScript -->
+  </head>
+
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <!-- Содержимое Navbar -->
+      </nav>
+    </div>
+
+    @yield('content')
+  </body>
+</html>
